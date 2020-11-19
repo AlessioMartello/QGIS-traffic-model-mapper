3a2c435fdd472d18acac356de658eafa745eb45e
Alessio Martello <alessio.martello@hotmail.co.uk>
2020-11-19 12:08:41 +0000

    Successfully grouped volumes to unique routes

--- methods.py
+++ methods.py
@@ -5,6 +5,7 @@ import json
 
 from constants import LINK_INPUT, LINK_OUTPUT, FAIL_OUTPUT
 
+
 def load_data(strategic_data_file, qgis_data_file):
     """ Reads Excel data into Pandas DataFrames"""
     strategic_raw_data = pd.read_excel(strategic_data_file, header=0, dtpe=object)
@@ -14,36 +15,36 @@ def load_data(strategic_data_file, qgis_data_file):
 
 def select_route_data(strategic_raw_data, ogv=None):
     """Obtain the relevant user class nodes"""
-    ogv_index=min(strategic_raw_data[strategic_raw_data["UC"] == 9].index)
+    ogv_index = min(strategic_raw_data[strategic_raw_data["UC"] == 9].index)
     if ogv:
-        strategic_data=strategic_raw_data[ogv_index:]
-        volume_data=strategic_data[strategic_data.iloc[:,0] != "route"]["Flow"].dropna().round(decimals=2)
+        strategic_data = strategic_raw_data[ogv_index:]
+        volume_data = strategic_data[strategic_data.iloc[:, 0] != "route"]["Flow"].dropna().round(decimals=2)
     else:
-        strategic_data=strategic_raw_data[:ogv_index]
-        volume_data=strategic_data[strategic_data.iloc[:,0] != "route"]["Flow"].dropna().round(decimals=2)
+        strategic_data = strategic_raw_data[:ogv_index]
+        volume_data = strategic_data[strategic_data.iloc[:, 0] != "route"]["Flow"].dropna().round(decimals=2)
 
-    strategic_data=strategic_data[strategic_data.iloc[:,0] == "route"]
+    strategic_data = strategic_data[strategic_data.iloc[:, 0] == "route"]
     return volume_data, strategic_data
 
 
 def to_list(strategic_data):
     # Create a list with all the nodes
     nodes = strategic_data.to_string(header=False, index=False).split()
-    nodes=list(filter(lambda x: "NaN" not in x, nodes))
+    nodes = list(filter(lambda x: "NaN" not in x, nodes))
     return nodes
 
 
 def group_nodes(nodes):
     """Create a nested list containing lists of nodes that make up each route. Use "route" string as separator."""
-    
-    nodes_grouped, count = [], -1 # todo change from -1
+
+    nodes_grouped, count = [], -1  # todo change from -1
     for i in range(len(nodes)):
         if nodes[i] == "route":
             nodes_grouped.append([])
-            count+=1
+            count += 1
             continue
         if "+" in nodes[i]:
-            nodes_grouped[count].append(float(nodes[i].replace("+",""))) # Handle the extra characters
+            nodes_grouped[count].append(float(nodes[i].replace("+", "")))  # Handle the extra characters
         else:
             nodes_grouped[count].append(float(nodes[i]))
     return nodes_grouped
@@ -52,23 +53,24 @@ def group_nodes(nodes):
 def group_links(nodes_grouped):
     # Adjust the final element in each uple that have been formatted as % in excel
     for count, node_group in enumerate(nodes_grouped):
-        if len(str(node_group[-1])) < len(str(node_group[0])): # Think of a more robust way, perhaps average the lengths of all but the last
-            node_group[-1] = node_group[-1]*100
-        nodes_grouped[count] = list(map(int,node_group))
+        if len(str(node_group[-1])) < len(
+                str(node_group[0])):  # Think of a more robust way, perhaps average the lengths of all but the last
+            node_group[-1] = node_group[-1] * 100
+        nodes_grouped[count] = list(map(int, node_group))
 
     # In the same nested list format, populate with the node to node combinations that make up the links
-    links=[]
+    links = []
     for i in range(len(nodes_grouped)):
         links.append([])
-        for j in range(len(nodes_grouped[i])-1):
-            links[i].append(f"{nodes_grouped[i][j]}>{nodes_grouped[i][j+1]}")
+        for j in range(len(nodes_grouped[i]) - 1):
+            links[i].append(f"{nodes_grouped[i][j]}>{nodes_grouped[i][j + 1]}")
     return links
 
 
 def obtain_routes(links, qgis_table):
     # Extract the ID for each node to node combination, representing a link.
     # Obtain the link numbers for each node to node combination. Each list is a route composed of links.
-    routes=[]
+    routes = []
     for i in range(len(links)):
         routes.append([])
         for link in links[i]:
@@ -77,8 +79,11 @@ def obtain_routes(links, qgis_table):
     return routes
 
 
-def qgis_json_format(unique_routes, volumes, ogv=None,  LINK_INPUT=LINK_INPUT):
+def qgis_json_format(routes_df=pd.DataFrame([]), ogv=None, LINK_INPUT=LINK_INPUT):
     """Format the sequence of links to be a list of dictionaries accepted by qgis"""
+    unique_routes = [i.strip("[]").split(",") for i in routes_df["Routes"]]
+    volumes = list(routes_df["Volumes"])
+
     def define_outputs(LINK_OUTPUT=LINK_OUTPUT, FAIL_OUTPUT=FAIL_OUTPUT):
         if not ogv:
             route_output = f"{LINK_OUTPUT}/{unique_routes[i][0]}_{i}_1_{volumes[i]}.gpkg"
@@ -90,28 +95,31 @@ def qgis_json_format(unique_routes, volumes, ogv=None,  LINK_INPUT=LINK_INPUT):
 
     qgis_route_list, route_ids = [], []
     for i in range(len(unique_routes)):
-        route={}
-        route["PARAMETERS"] ={}
+        route = {}
+        route["PARAMETERS"] = {}
         route["PARAMETERS"]["INPUT"] = LINK_INPUT
-        route["PARAMETERS"]["EXPRESSION"] = "' \\\"ID\\\"  = " + " or \\\"ID\\\"  = ".join(list(map(str,unique_routes[i]))) + "\\n'"
+        route["PARAMETERS"]["EXPRESSION"] = "' \\\"ID\\\"  = " + " or \\\"ID\\\"  = ".join(
+            list(map(str, unique_routes[i]))) + "\\n'"
         route["OUTPUTS"] = {}
         route["OUTPUTS"]["OUTPUT"], route["OUTPUTS"]["FAIL_OUTPUT"] = define_outputs()
         route_ids.append(define_outputs()[0])
         qgis_route_list.append(route)
     return qgis_route_list, route_ids
 
+
 def export_to_json(filename, data):
     file = f"{filename}.json"
     with open(file, "w") as f:
         json.dump(data, f)
 
-def create_volume_table(route_codes,  unique_routes_list, all_volumes = []):
-    for route_code, links, volumes, names in zip(route_codes, unique_routes_list, all_volumes, ["Volumes", "OGV_Volumes"]):
+
+def create_volume_table(route_codes, unique_routes_df, all_volumes):
+    for route_code, links, volumes, names in zip(route_codes, unique_routes_df, all_volumes,
+                                                 ["Volumes", "OGV_Volumes"]):
         route_codes_id_df = pd.DataFrame(route_code).reset_index(drop=True)
         route_codes_id_df.columns = [f"Origin_Route-ID_UC_Volume"]
-        links_df = pd.DataFrame(links).reset_index(drop=True)
-        volumes_df = pd.DataFrame(volumes).reset_index(drop=True)
+        links_df = links.reset_index(drop=True)
+        volumes_df = volumes.reset_index(drop=True)
         volumes_df.columns = [f"Volume"]
-        volume_results= pd.concat([route_codes_id_df, links_df, volumes_df], axis=1)
+        volume_results = pd.concat([route_codes_id_df, links_df, volumes_df], axis=1)
         volume_results.to_excel(f"{names}.xlsx", index=False)
-
--- run.py
+++ run.py
@@ -12,7 +12,7 @@ ogv_volumes, ogv_nodes = methods.select_route_data(strategic_raw_data,
 
 data_sets = {"routes": nodes, "ogv_routes": ogv_nodes}
 volume_data_sets = {"volumes": volumes, "ogv_volumes": ogv_volumes}
-route_codes, all_routes_list = [], []
+route_codes, all_routes_list, all_volumes = [], [], []
 for (user_class, data), (volume) in zip(data_sets.items(), volume_data_sets.values()):
     # Block executes code for JSON file to be imported into QGIS
     volume = volume.tolist()
@@ -20,18 +20,18 @@ for (user_class, data), (volume) in zip(data_sets.items(), volume_data_sets.valu
     nodes_grouped = methods.group_nodes(nodes)  # Group the node sequences that make up a route
     links = methods.group_links(nodes_grouped)  # From the node sequences create the links
     routes = methods.obtain_routes(links, qgis_table)  # For the links create the list of links that make up each route
+
     routes_df = pd.DataFrame(pd.Series(list(map(str,routes))), columns=["Routes"])
     routes_df["Volumes"] = pd.Series(volume, index=routes_df.index)
-    routes_df=routes_df.groupby("Routes").sum()
-    routes_df.to_excel(f"{user_class}grouped volumes.xlsx")
-    qgis_routes, route_ids = methods.qgis_json_format(routes,
-                                                      volume) if user_class != "ogv_routes" else methods.qgis_json_format(
-        routes, volume, ogv=True)  # Format results
+    routes_df = routes_df.groupby("Routes", as_index=False).sum().round(decimals=2)
+
+    qgis_routes, route_ids = methods.qgis_json_format(routes_df) if user_class != "ogv_routes" else methods.qgis_json_format(routes_df, ogv=True)  # Format results
     methods.export_to_json(user_class, qgis_routes)  # Export json files
 
     # Block executes code for the Excel table of results
-    all_routes_list.append(routes)  # List of all routes, before dropping duplicates, used for volume results
-    route_codes.append([route_ids[i].split("/")[-1].strip(".gpkg") + "_" + str(volume[i]) for i in range(len(routes))])
+    all_routes_list.append(routes_df["Routes"])  # List of all routes, before dropping duplicates, used for volume results
+    all_volumes.append(routes_df["Volumes"])
+    route_codes.append([route_ids[i].split("/")[-1].strip(".gpkg") for i in range(len(route_ids))])
 
 methods.create_volume_table(route_codes, all_routes_list,
-                            all_volumes=[volumes, ogv_volumes])  # Create a table of routes and volumes and export it
+                            all_volumes=all_volumes)  # Create a table of routes and volumes and export it
