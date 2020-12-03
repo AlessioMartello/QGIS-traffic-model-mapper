7480d69654c82dd83cd54b882ff9165d41333964
Alessio Martello <alessio.martello@hotmail.co.uk>
2020-12-03 21:11:34 +0000

    button colour updates when files is selected

--- gui.py
+++ gui.py
@@ -7,7 +7,6 @@ import pathlib
 
 root = tk.Tk()
 root.title("Strategic route mapper")
-# root.geometry("650x300")
 BACKGROUND_COLOUR = "#d6d6d6"
 FOREGROUND_COLOUR = "#512d6d"
 
@@ -21,19 +20,26 @@ class RouteMapper:
         self.error_message = None
         self.intro = tk.Label(main, text="Select the files containing the strategic data, GIS data and then click run:",
                               fg=FOREGROUND_COLOUR, font=("", 15, "bold"), padx=20, pady=10).pack()
+
         self.strategic_data = tk.Button(main, text="Choose strategic data file", command=lambda: self.choose_data(0),
                                         bg=BACKGROUND_COLOUR,
-                                        fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15)).pack(
-            pady=20)
+                                        fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15))
+        self.strategic_data.pack(pady=20)
+
         self.gis_data = tk.Button(main, text="Choose GIS data file", command=lambda: self.choose_data(1),
                                   bg=BACKGROUND_COLOUR,
-                                  fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15)).pack(pady=20)
+                                  fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15))
+        self.gis_data.pack(pady=20)
         self.run_button = tk.Button(main, text="Run", command=self.run_analysis, bg=BACKGROUND_COLOUR,
                                 fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15)).pack(pady=20)
 
     def choose_data(self, index):
         self.data_files[index] = pathlib.Path(
             askopenfilename(title="Choose the strategic data file", filetypes=[("Excel files", ".xls .xlsx")]))
+        if index == 0:
+            self.strategic_data.config(fg="green")
+        else:
+            self.gis_data.config(fg="green")
 
     def run_analysis(self):
         try:
