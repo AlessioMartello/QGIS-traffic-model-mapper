fa08d76d9e933347b5ebd4c5fed7fd3066e12b02
Alessio Martello <alessio.martello@hotmail.co.uk>
2020-12-08 12:28:20 +0000

    refactored gui Class

--- gui.py
+++ gui.py
@@ -16,33 +16,37 @@ class RouteMapper:
     def __init__(self, main):
         myFrame = tk.Frame(main)
         myFrame.pack()
+
         self.data_files, self.directories = [None, None], [None, None, None]
         self.error_message = None
+
         self.intro = tk.Label(main, text="Select the files containing the strategic data, GIS data and then click run:",
                               fg=FOREGROUND_COLOUR, font=("", 15, "bold"), padx=20, pady=10).pack()
 
         self.strategic_data = tk.Button(main, text="Choose strategic data file", command=lambda: self.choose_data(0),
                                         bg=BACKGROUND_COLOUR,
                                         fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15))
-        self.strategic_data.pack(pady=20)
         self.gis_data = tk.Button(main, text="Choose GIS data file", command=lambda: self.choose_data(1),
                                   bg=BACKGROUND_COLOUR,
                                   fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15))
-        self.gis_data.pack(pady=20)
         self.link_input_btn = tk.Button(main, text="Select link input folder", command=lambda: self.choose_directory(0),
                                   bg=BACKGROUND_COLOUR,
                                   fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15))
-        self.link_input_btn.pack(pady=20)
         self.link_output_btn = tk.Button(main, text="Select link output folder", command=lambda: self.choose_directory(1),
                                   bg=BACKGROUND_COLOUR,
                                   fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15))
-        self.link_output_btn.pack(pady=20)
         self.link_fail_btn = tk.Button(main, text="Select link fail folder", command=lambda: self.choose_directory(2),
                                   bg=BACKGROUND_COLOUR,
                                   fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15))
-        self.link_fail_btn.pack(pady=20)
         self.run_button = tk.Button(main, text="Run", command=self.run_analysis, bg=BACKGROUND_COLOUR,
-                                fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15)).pack(pady=20)
+                                fg=FOREGROUND_COLOUR, highlightcolor=FOREGROUND_COLOUR, font=("", 15))
+
+        self.strategic_data.pack(pady=20)
+        self.gis_data.pack(pady=20)
+        self.link_input_btn.pack(pady=20)
+        self.link_output_btn.pack(pady=20)
+        self.link_fail_btn.pack(pady=20)
+        self.run_button.pack(pady=20)
 
     def choose_data(self, index):
         self.data_files[index] = pathlib.Path(
